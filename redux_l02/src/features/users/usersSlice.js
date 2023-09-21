4d40b2cf604713ce84a0e4a9b0f90136d3c589cf
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-09-21 16:43:10 +0530

    making api call when application starts

--- redux_l02/src/features/users/usersSlice.js
+++ redux_l02/src/features/users/usersSlice.js
@@ -1,15 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [
-  { id: '1', name: 'Dude Lebowski' },
-  { id: '2', name: 'Neil Young' },
-  { id: '3', name: 'Dave Gray' },
-];
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+// const initialState = [
+//   { id: '1', name: 'Dude Lebowski' },
+//   { id: '2', name: 'Neil Young' },
+//   { id: '3', name: 'Dave Gray' },
+// ];
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    const response = await axios.get(USERS_URL);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const userSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {});
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {});
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
